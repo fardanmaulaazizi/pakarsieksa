cc02010fd5d4e38b71080b8f8b8f9a9a435e2e92
fardanmaulaazizi <fardanm.azizi11@gmail.com>
2024-11-07 15:50:05 +0700

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,30 +1,26 @@
+import { Mail } from "lucide-react";
 import Link from "next/link";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import { menu } from "@/data/menu";
 
 export default function Footer() {
   return (
     <footer className="bg-primary text-black mt-16">
       <div className="container mx-auto px-4 py-8 max-w-6xl">
-        <div className="flex flex-col text-2xl justify-between items-center gap-8">
-          <h3 className="text-xl font-bold mb-4 text-yellow">
-            Tentang Pakarxie
-          </h3>
-          <p className="text-white text-sm text-center">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad nisi
-            beatae saepe dignissimos, officiis tenetur consectetur, harum
-            necessitatibus commodi ut ea. Rem fugit, assumenda quis dicta iure
-            voluptate nesciunt consequatur laborum eos nihil quidem cupiditate
-            voluptas similique aspernatur mollitia omnis nobis odio sint
-            dolorem. Nihil voluptatum dolores, dignissimos quae maiores ducimus
-            aperiam sapiente? Impedit, unde.
+        <div className="flex flex-col text-2xl justify-between items-center ">
+          <p className="text-white text-lg text-center">
+            10.000+ Jenis Alat Keperkakasan , Esok Penghantaran ke awak.
           </p>
-          <p className="text-yellow text-sm text-center">
-            Email: tianyuanhardware@gmail.com
+          <h3 className="text-xl font-bold text-yellow  mb-5">- Pakarxie -</h3>
+          <p className="text-yellow text-sm text-center hover:underline ">
+            <Link
+              href="emailto:tianyuanhardware@gmail.com"
+              className="flex items-center gap-2 text-lg"
+            >
+              <Mail className="h-5" />
+              Email: tianyuanhardware@gmail.com
+            </Link>
           </p>
         </div>
-        <div className="mt-8 pt-8 border-t border-slate-100 text-center">
+        <div className="mt-8  border-t border-slate-100 text-center">
           <p className="text-sm text-yellow font-bold">
             &copy; 2024 Pakarxie. All rights reserved.
           </p>
